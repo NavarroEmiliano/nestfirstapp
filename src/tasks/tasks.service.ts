3e9ef540a23e63a104202c72cb4e511d6a591e0d
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-07-03 14:57:55 -0300

    delete

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.entity';
+import { v4 } from 'uuid';
+import { UpdateTaskDTO } from './dto/task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [
+    {
+      id: '1',
+      title: 'first task',
+      description: 'some task',
+      status: TaskStatus.PENDING,
+    },
+  ];
+
+  getAllTasks() {
+    return this.tasks;
+  }
+  createTask(title: string, description: string): Task {
+    const task = {
+      id: v4(),
+      title,
+      description,
+      status: TaskStatus.PENDING,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+  deleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+  updateTask(id: string, updatedFields: UpdateTaskDTO): Task {
+    const task = this.getTaskById(id);
+    const updatedTask = Object.assign(task, updatedFields);
+    this.tasks = this.tasks.map((t) => (t.id === id ? updatedTask : t));
+    return updatedTask;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+}
